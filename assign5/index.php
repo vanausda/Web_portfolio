a76b37a25f18a9d0e584369b1de959be0ec5b711
vanausda <sev15@my.fsu.edu>
2018-01-25 09:55:33 -0500

    Create index.php

---
+++ assign5/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+body {
+    background-color: #eaecee;
+}
+
+#header {
+    height: 200px;
+    text-align: center;
+}
+
+#return_port {
+    text-align: center;
+
+}
+
+.div {
+height: 100px;
+
+}
+
+#nav {
+padding-right: 30px;
+height: 200px;
+text-align: right;
+margin: 20px;
+
+}
+
+a { padding-right: 30px; }
+a:visited { color: #1364c9; }
+a:hover { color: #e73030; }
+a:active { color: #fffff; }
+
+#bottom_div {
+width: 100%; 
+height: 200px;
+background-color: #c8ccd1;
+text-align: center;
+margin: auto;
+
+}
+</style>
+</head>
+<body>
+<nav id="nav"	>
+<a href="https://investorplace.com/">Go to InvestorPlace</a>
+<a href="login.php">Log In</a>
+
+</nav>
+<header id="header"><h1>UNF Stock Finder</h1></header>
+
+<div id="top"></div>
+<div id="bottom_div">
+
+<a id="return_port" href="https://vanausda.github.io/Web_portfolio/">Return to portfolio</a>
+</div>
+
+
+</body>
+</html>
